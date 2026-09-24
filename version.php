e6a7e789997f3a51d63868ba86e3d07566184799
rollercly <rollercly@gmail.com>
2026-09-24 11:14:25 +0200

    actualizo version

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_strava';
-$plugin->version   = 2026091401;      // YYYYMMDDXX
+$plugin->version   = 2026092400;      // YYYYMMDDXX
 $plugin->requires  = 2022112800;      // Moodle 4.1+
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1.0';
 $plugin->dependencies = [
-    'local_stravaauth' => 2026091000,
+    'local_stravaauth' => 2026092400,
 ];
